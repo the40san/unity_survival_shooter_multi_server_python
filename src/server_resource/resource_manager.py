94419d4594749f85583398eab670a90535f424ac
Masashi AKISUE <masashi.akisue@aktsk.jp>
2017-04-10 05:51:00 +0900

    add message id generator

--- src/server_resource/resource_manager.py
+++ src/server_resource/resource_manager.py
@@ -13,25 +13,36 @@ class ResourceManager:
         pass
 
     def initialize(self):
-        self.lock = threading.Lock()
+        self.lock_for_resource = threading.Lock()
+        self.lock_for_message = threading.Lock()
         self.bots = set([])
         self.players = set([])
         self.latest_player_id = 0
+        self.latest_message_id = 1
 
     def add_player(self, new_player):
-        self.lock.acquire()
+        self.lock_for_resource.acquire()
 
         self.players.add(new_player)
 
-        self.lock.release()
+        self.lock_for_resource.release()
 
     def create_player_id(self):
-        self.lock.acquire()
+        self.lock_for_resource.acquire()
 
         self.latest_player_id += 1
 
-        self.lock.release()
+        self.lock_for_resource.release()
         return self.latest_player_id
+
+    def create_new_message_id(self, num = 1):
+        self.lock_for_message.acquire()
+
+        begin = self.latest_message_id + 1
+        self.latest_message_id += num
+
+        self.lock_for_message.release()
+        return begin
 
     @classmethod
     def instance(cls):
