bbed98d65da5eeb0843d1de2c0a9116b3d290dd7
Masashi AKISUE <masashi.akisue@aktsk.jp>
2017-04-10 05:56:11 +0900

    split svack svplayerjoin

--- src/message/sv_player_join.py
+++ src/message/sv_player_join.py
@@ -2,11 +2,11 @@ from message.sv_response_base import SvResponseBase
 from server_resource.resource_manager import ResourceManager
 import struct
 
-class SvAck(SvResponseBase):
+class SvPlayerJoin(SvResponseBase):
     def __init__(self, cl_request):
         super(SvAck, self).__init__(cl_request)
-        self.message_id = 200
+        self.message_id = 201
 
     def payload(self):
         new_message_id = ResourceManager.instance().create_new_message_id()
-        return struct.pack("<BHH", self.message_id, new_message_id, self.cl_request.message_unique_id)
+        return struct.pack("<BHH", self.message_id, new_message_id, cl_request.new_player_id)
